4675c1f0947a8b1cd09f7f72bcbeb3facd59edf7
unknown <mohammadfahadamajd@gmail.com>
2023-01-20 01:40:53 +0500

    added new version of website, not complete yet though

---
+++ appear-animation.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll(".come-out");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((e) => {
+      e.target.classList.toggle("appear", e.isIntersecting);
+      if (e.isIntersecting) observer.unobserve(e.target);
+    });
+    // console.log(entries);
+  },
+  {
+    threshold: 0.3,
+  }
+);
+elements.forEach((i) => {
+  observer.observe(i);
+});
--- script.js
+++
@@ -1,6 +0,0 @@
-
-//api key for gify
-// BCmE9PDcD9CW6YPukMjqvCH627h3UQWH
-
-// api key for open weather
-//    6f6cb1bab4804f6034ce0c50ea947c73
